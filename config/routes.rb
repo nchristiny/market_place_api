914d48cdbd87b7d56a2b6fa7aadad0ad5d595f8d
nchristiny <icarus747@gmail.com>
2016-06-12 17:24:23 -0500

    Implement User#create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, :only => [:show, :index]
+      resources :users, :only => [:show, :index, :create]
     end
   end
   devise_for :users
