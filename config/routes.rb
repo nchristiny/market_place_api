1a549781b0883e7c9a804e13e6b8548863ccdfb0
nchristiny <icarus747@gmail.com>
2016-06-12 17:31:16 -0500

    Implement User#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, :only => [:show, :index, :create, :update]
+      resources :users, :only => [:show, :index, :create, :update, :destroy]
     end
   end
   devise_for :users
