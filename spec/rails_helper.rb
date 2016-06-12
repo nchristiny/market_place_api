8ffcc43d2d070acf4043327edf513835b87757e7
nchristiny <icarus747@gmail.com>
2016-06-12 17:59:31 -0500

    Test header spec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -43,6 +43,12 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.include Request::JsonHelpers, :type => :controller
+
+  config.include Request::HeadersHelpers, :type => :controller
+
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
