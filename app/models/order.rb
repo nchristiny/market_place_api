344115bc254ee753958f9ae50398cc660e146354
nchristiny <icarus747@gmail.com>
2016-06-14 16:28:32 -0500

    Add orders#create action in controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,15 @@
 class Order < ActiveRecord::Base
-  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  before_validation :set_total!
+
+  # Not needed due to #set_total! method
+  # validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
 
   belongs_to :user
   has_many :placements
   has_many :products, through: :placements
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
