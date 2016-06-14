4b38911b45b184051c013e5419212f8312a8d93a
nchristiny <icarus747@gmail.com>
2016-06-14 16:20:00 -0500

    Add order#show action to controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::OrdersController < ApplicationController
+  before_action :authenticate_with_token!
 
   respond_to :json
 
   def index
     respond_with current_user.orders
+  end
+
+  def show
+    respond_with current_user.orders.find(params[:id])
   end
 end
