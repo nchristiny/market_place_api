931075beb80403da270d090b3ac78ae1bb9b48f6
nchristiny <icarus747@gmail.com>
2016-06-14 16:08:50 -0500

    Associate product and order with placement join model

---
+++ app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ActiveRecord::Base
+  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :user_id, presence: true
+
+  belongs_to :user
+  has_many :placements
+  has_many :products, through: :placements
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe Product, type: :model do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   it { should validate_presence_of :user_id }
   it { should belong_to :user }
+  it { should have_many(:placements) }
+  it { should have_many(:orders).through(:placements) }
 
   describe ".filter_by_title" do
     before(:each) do
