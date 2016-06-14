c504701d97526e4425f3f538ee97b1633407d38f
nchristiny <icarus747@gmail.com>
2016-06-14 18:18:06 -0500

    Add delayed jobs

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'active_model_serializers', '0.8.3'
 gem 'devise'
 # Pagination not implemented
 # gem 'kaminari'
+gem 'delayed_job_active_record'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,11 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Perform mailer delivery in development environment
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = { :host => "localhost", only_path: false }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
